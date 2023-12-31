37b71304f8f7e15a723d824f83248aa2218d1845
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2023-12-31 12:25:12 +0200

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -6,15 +6,12 @@ from config import SUPPORT_CHAT,OWNER_ID
 keyboard = InlineKeyboardMarkup(
     [
         [InlineKeyboardButton(text="بدا استخراج الجلسة 🖥️", callback_data="gensession")],
-        ],
-                    [
+        [
             InlineKeyboardButton(text="سـورس مـحـمـود - Mahmoud", url="t.me/YY5Y8"),
-            ],
-                    [
+            [
             InlineKeyboardButton(text="مـعـلـومات الـمـطـور", url="t.me/S9_FG"),
-            ],
-                    [
-            InlineKeyboardButton("الـمـطـور👷", user_id=OWNER_ID),
+            [
+            InlineKeyboardButton("الـمـطـور👷", user_id=OWNER_ID)
                     InlineKeyboardButton(text="اوامـر الـبـوت", url="https://telegra.ph/%D8%A7%D9%88%D8%A7%D9%85%D8%B1-%D8%A8%D9%88%D8%AA-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%AE%D8%B1%D8%A7%D8%AC-10-11")
             ),
         ],
