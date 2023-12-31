1e22396e410e7fcdb1d110915f9bf1bf9c53526d
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2023-12-31 20:23:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5488301592))
+OWNER_ID = int(getenv("OWNER_ID", 1095638828))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YY5Y8")
 
 MUST_JOIN = getenv("MUST_JOIN", None)
